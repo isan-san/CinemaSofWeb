f4d71d4bbf210ffbdc0b03fc1a1308bc0eeebbc8
Isan Studio1642 <isan@studio1642.com>
2023-04-08 18:04:14 -0500

    Add filtering method to show list componet

--- src/components/container/shows-list/shows-list.component.ts
+++ src/components/container/shows-list/shows-list.component.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { Show } from 'src/models/showModel';
 import { ShowService } from 'src/services/show.service.service';
 
 @Component({
   selector: 'app-shows-list',
   templateUrl: './shows-list.component.html',
-  styleUrls: ['./shows-list.component.scss']
+  styleUrls: ['./shows-list.component.scss'],
 })
 export class ShowsListComponent implements OnInit {
-
   shows: Show[] = [];
+  filteredShows: Show[] = [];
+  @Input() filter: string = '';
 
-  constructor(private showService: ShowService) { }
+  constructor(private showService: ShowService) {}
 
   ngOnInit(): void {
-    this.showService.getAllShows().subscribe(shows => this.shows = shows);
+    this.showService.getAllShows().subscribe((shows) => {
+      this.shows = shows;
+      this.filteredShows = shows;
+    });
   }
-
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['filter']) {
+    }
+  }
+  filterShows(filter: string, shows: Show[]) {
+    filter.toLowerCase
+    this.filteredShows = this.shows.filter(show=>{
+      return show.categories.find(categorie=>{
+        categorie.toLowerCase
+        return categorie.includes(filter);
+      })
+    })
+  }
 }
