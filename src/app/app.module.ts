abba1a69c55c72c9453d0c586d772a5592c0e5fa
Isan Studio1642 <isan@studio1642.com>
2023-04-08 16:20:35 -0500

    Update imports in app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShowsListComponent } from './container/shows-list/shows-list.component';
+import { PurchaseTicketFormComponent } from './container/purchase-ticket-form/purchase-ticket-form.component';
+import { ShowComponent } from './pure/show/show.component';
+import { TicketComponent } from './pure/ticket/ticket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowsListComponent,
+    PurchaseTicketFormComponent,
+    ShowComponent,
+    TicketComponent
   ],
   imports: [
     BrowserModule,
