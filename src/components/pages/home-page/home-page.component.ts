7999bad945d6c866624e99538ef1690e1e25c37d
Isan Studio1642 <isan@studio1642.com>
2023-04-08 18:03:35 -0500

    Add basic structure to home page component

--- src/components/pages/home-page/home-page.component.ts
+++ src/components/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ShowsListComponent } from 'src/components/container/shows-list/shows-list.component';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent {
-
+  filter: string = '';
 }
