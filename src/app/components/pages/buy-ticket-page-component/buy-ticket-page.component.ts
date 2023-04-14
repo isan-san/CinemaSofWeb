cce1e3c3390367badcc342c294af37f916e70c4d
Isan Studio1642 <isan@studio1642.com>
2023-04-14 09:13:20 -0500

    Add buy ticket page

--- src/app/components/pages/buy-ticket-page-component/buy-ticket-page.component.ts
+++ src/app/components/pages/buy-ticket-page-component/buy-ticket-page.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FormValues } from '../../pure/buy-ticket-form/buy-ticket-form.component';
-const formData = require('form-data');
-import * as Mailgun from 'mailgun-js';
+import { TicketService } from 'src/services/ticket.service.service';
+import { ShowService } from 'src/services/show.service.service';
+import { Show } from 'src/models/showModel';
+import { Ticket } from 'src/models/ticketModel';
+// const formData = require('form-data');
+// import * as Mailgun from 'mailgun-js';
 
 @Component({
   selector: 'app-buy-ticket-page-component',
@@ -10,29 +14,56 @@ import * as Mailgun from 'mailgun-js';
   styleUrls: ['./buy-ticket-page-component.component.scss'],
 })
 export class BuyTicketPageComponent {
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private ticketService: TicketService,
+    private showService: ShowService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
   showId: string = '';
-  mailgun: Mailgun.Mailgun | undefined = undefined;
+  show: Show | undefined = undefined;
+  ticket: Ticket | undefined = undefined;
+  // mailgun: Mailgun.Mailgun | undefined = undefined;
 
   ngOnInit() {
     this.showId = this.route.snapshot.paramMap.get('showId') as string;
-    this.mailgun = new Mailgun({
-      apiKey: 'aa4b61d7f0a72b494a9467b0bf90f893-2cc48b29-27fcb427',
-      domain:
-        'https://api.mailgun.net/v3/sandboxeb921424f3cb463a98b8aecd9bd103b0.mailgun.org',
+    this.showService.getShowById(this.showId).subscribe((show) => {
+      this.show = show;
     });
-
+    // this.mailgun = new Mailgun({
+    //   apiKey: 'aa4b61d7f0a72b494a9467b0bf90f893-2cc48b29-27fcb427',
+    //   domain:
+    //     'https://api.mailgun.net/v3/sandboxeb921424f3cb463a98b8aecd9bd103b0.mailgun.org',
+    // });
   }
 
   buyTicket(data: FormValues) {
-      this.mailgun?.post('sandbox-123.mailgun.org', {
-        from: "Excited User <mailgun@sandbox-123.mailgun.org>",
-        to: ["isan.9.9f@gmail.com"],
-        subject: "Hello",
-        text: "Testing some Mailgun awesomeness!",
-        html: "<h1>Testing some Mailgun awesomeness!</h1>"
-      })
-      .then(msg => console.log(msg)) // logs response data
-      .catch(err => console.log(err));
+    if(confirm("Are sou shure you want to by a ticket for "+this.show?.title)){
+    if ((this.show!).availableTickets > 0) {
+      this.ticketService
+        .saveTicket({
+          show: this.show,
+        } as Ticket)
+        .subscribe((ticket) => {
+          this.ticket = ticket;
+          this.show!.availableTickets--;
+          this.showService.saveShow(this.show!).subscribe();
+        });
+    }else{
+      alert("There is no avaalable tickets, come back latter.")
+    }
+  }
+    // this.mailgun?.post('sandbox-123.mailgun.org', {
+    //   from: "Excited User <mailgun@sandbox-123.mailgun.org>",
+    //   to: ["isan.9.9f@gmail.com"],
+    //   subject: "Hello",
+    //   text: "Testing some Mailgun awesomeness!",
+    //   html: "<h1>Testing some Mailgun awesomeness!</h1>"
+    // })
+    // .then(msg => console.log(msg)) // logs response data
+    // .catch(err => console.log(err));}
+  }
+  goToShows(){
+    this.router.navigate(['/home']);
   }
 }
--- src/app/components/pure/buy-ticket-form/buy-ticket-form.component.ts
+++ src/app/components/pure/buy-ticket-form/buy-ticket-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 export interface FormValues {
   name: string;
@@ -12,16 +13,16 @@ export interface FormValues {
 @Component({
   selector: 'app-buy-ticket-form',
   templateUrl: './buy-ticket-form.component.html',
-  styleUrls: ['./buy-ticket-form.component.scss']
+  styleUrls: ['./buy-ticket-form.component.scss'],
 })
 export class BuyTicketFormComponent {
-
-  name: string = "";
-  email: string = "";
-  selectedPaymentMethod: string = "";
-  cardNumber: string = "";
-  paypalEmail: string = "";
-  bitcoinAddress: string = "";
+  constructor(private router: Router) {}
+  name: string = '';
+  email: string = '';
+  selectedPaymentMethod: string = '';
+  cardNumber: string = '';
+  paypalEmail: string = '';
+  bitcoinAddress: string = '';
 
   @Output() formSubmitted = new EventEmitter<FormValues>();
 
@@ -30,14 +31,22 @@ export class BuyTicketFormComponent {
       name: this.name,
       email: this.email,
       paymentMethod: this.selectedPaymentMethod,
-      cardNumber: this.selectedPaymentMethod === 'credit' ? this.cardNumber : undefined,
-      paypalEmail: this.selectedPaymentMethod === 'paypal' ? this.paypalEmail : undefined,
-      bitcoinAddress: this.selectedPaymentMethod === 'bitcoin' ? this.bitcoinAddress : undefined
+      cardNumber:
+        this.selectedPaymentMethod === 'credit' ? this.cardNumber : undefined,
+      paypalEmail:
+        this.selectedPaymentMethod === 'paypal' ? this.paypalEmail : undefined,
+      bitcoinAddress:
+        this.selectedPaymentMethod === 'bitcoin'
+          ? this.bitcoinAddress
+          : undefined,
     };
     this.formSubmitted.emit(formValues);
   }
 
   selectPaymentMethod(paymentMethod: string) {
     this.selectedPaymentMethod = paymentMethod;
+  }
+  goToShows() {
+    this.router.navigate(['/home']);
   }
 }
